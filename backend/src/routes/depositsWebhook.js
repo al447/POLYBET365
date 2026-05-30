437116d4057e9582c6b710060e9d69fb40a9575f
Peony001 <peony.k007@gmail.com>
2026-05-30 15:57:48 +0300

    remove old codebase

--- backend/src/routes/depositsWebhook.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { moonpayWebhook } = require('../controllers/depositController');
-
-// MoonPay webhook with raw body already attached
-router.post('/', moonpayWebhook);
-
-module.exports = router;
